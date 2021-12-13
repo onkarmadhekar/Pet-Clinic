a496fc97cc572b883cabb4ba8fc5f1e46ca68c4e
Onkar Madhekar <onkarmadhekar22@gmail.com>
2021-12-13 11:56:33 +0530

    Implemented Map. Closes #6. Closes #8

---
+++ pet-clinic-web/src/main/java/onkar/springframework/petclinic/contoller/IndexController.java
@@ -0,0 +1,13 @@
+package onkar.springframework.petclinic.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String index(){
+        return "index";
+    }
+}
